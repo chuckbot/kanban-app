97006868c1c81aaa8fd66b4664736a65dbe37d68
Carlos Garcia <cgmilan@gmail.com>
2020-12-26 23:58:47 -0400

    Card Component, Render Children Inside The Columns

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { ColumnContainer, ColumnTitle } from "./styles";
 
-export const Column = () => {
-    return <div>Colum Title</div>
+interface ColumnProps {
+  text: string;
 }
+
+export const Column = ({
+  text,
+  children,
+}: React.PropsWithChildren<ColumnProps>) => {
+  return (
+    <ColumnContainer>
+      <ColumnTitle>{text}</ColumnTitle>
+      {children}
+    </ColumnContainer>
+  );
+};
