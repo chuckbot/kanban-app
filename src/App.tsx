4582436c44462f234bf262dd1b7ac8c30a5e384c
Carlos Garcia <cgmilan@gmail.com>
2021-02-19 11:11:57 -0500

    Context and Add List

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from "react";
 import { Column } from "./Column";
-import { Card } from "./Card";
-import { AddNewItem } from "./AdNewItem";
+import { AddNewItem } from "./AddNewItem";
 import { AppContainer } from "./styles";
+import { useAppState } from "./AppStateContext";
 
 const App = () => {
+  const { state } = useAppState();
+
   return (
     <AppContainer>
-      <Column text="To Do">
-        <Card text="Generate app scaffold"/>
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript"/>
-      </Column>
-      <Column text="Done">
-        <Card text="Begin to use static typing"/>
-      </Column>
-      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log}/>
+      {state.lists.map((lists, i) => (
+        <Column text={lists.text} key={lists.id} index={i} />
+      ))}
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </AppContainer>
   );
-}
+};
 
 export default App;
